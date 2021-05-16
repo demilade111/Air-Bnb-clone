657e19823caa38d60967047cda4f95f697d3077c
demilade <demiladealuko111@gmail.com>
2021-05-16 13:00:57 +0100

    airbnb clone

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Footer from "./Component/Footer";
+import Header from "./Component/Header";
+import Home from "./Component/Home";
+import SearchPage from "./Component/SearchPage";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Router>
+        <Header />
+
+        <Switch>
+          <Route exact path="/search">
+            <SearchPage/>
+          </Route>
+          <Route exact path="/">
+            <Home />
+          </Route>
+        </Switch>
+
+        <Footer />
+      </Router>
     </div>
   );
 }
---
+++ src/Component/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./Footer.css";
+
+function Footer() {
+    return (
+        <footer>
+        <p>© 2021 Airbnb, Clone.No right reserved - this is a demo</p>
+        <p>Privacy.Terms.SiteMap.Company Details</p>
+      
+        </footer>
+    )
+}
+
+export default Footer
